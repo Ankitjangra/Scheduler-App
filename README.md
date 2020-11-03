033d4233dae82945ad0d2feacfec16929834024c
Ankit Jangra <jangraankit112@gmail.com>
2020-11-03 15:35:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Scheduler App
+username:admin
+password:pass"# Scheduler-App"
+"# Scheduler-App" 
---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+const Contact = () => {
+    return (
+
+        <div className="container align-center">
+            <ul class="collection">
+                <li class="collection-item avatar">
+                    <img src="./jangra-crop.jpg" alt="" className="circle" />
+                    <span class="title">Ankit Jangra</span>
+                    <p>Frontend developer <br />
+             Awesome communiction skills and sound technical knowledge
+          </p>
+                </li>
+            </ ul>
+
+        </div>
+    );
+};
+export default Contact;
+
